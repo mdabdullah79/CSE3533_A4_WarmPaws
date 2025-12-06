694bb3c37df4062e8e0509e5445a67111c1cf8b8
mdabdullah79 <c233079@ugrad.iiuc.ac.bd>
2025-12-06 19:34:32 +0600

    email

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -12,7 +12,7 @@ const PrivateRoutes = ({children}) => {
 
     }
 
-    if(user && user?.email){
+    if(user){
         return children;
     }
     return <Navigate state={Location.pathname} to='/auth/login'></Navigate>
